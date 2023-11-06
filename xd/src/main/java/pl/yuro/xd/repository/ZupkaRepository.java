03f21464fb4b5ff9f2cb5991a443fb1fe60716d2
yuroDeeps <yurodeeps@protonmail.com>
2023-11-06 18:59:50 +0100

    Add spring boot set up and CRUD rest mapping

---
+++ xd/src/main/java/pl/yuro/xd/repository/ZupkaRepository.java
@@ -0,0 +1,7 @@
+package pl.yuro.xd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.yuro.xd.model.Zupka;
+
+public interface ZupkaRepository extends JpaRepository<Zupka, Long> {
+}
---
+++ xd/src/test/java/pl/yuro/xd/XdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.yuro.xd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
