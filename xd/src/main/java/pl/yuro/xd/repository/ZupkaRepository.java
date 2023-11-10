a26531e1dd8afcfb6d54674a996c0a2327f72eec
yuroDeeps <yurodeeps@protonmail.com>
2023-11-10 11:13:58 +0100

    Delete xd directory

--- xd/src/main/java/pl/yuro/xd/repository/ZupkaRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.yuro.xd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.yuro.xd.model.Zupka;
-
-public interface ZupkaRepository extends JpaRepository<Zupka, Long> {
-}
